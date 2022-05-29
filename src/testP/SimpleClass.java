d7bc13a4f2a42a6905e49859f6cb4742f9a45227
Your Name <you@example.com>
2022-05-29 12:46:49 -0400

    for class on 29th May

--- src/testP/SimpleClass.java
+++ src/testP/SimpleClass.java
@@ -1,9 +1,23 @@
 package testP;
 
+import java.util.Scanner;
+
 public class SimpleClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		double loanamount = 0;
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Please enter initial amount:");
+		loanamount = scn.nextDouble();
+
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("Month " + i + " payment =" + 0.1 * loanamount);
+			loanamount = (double) (loanamount - 0.1 * loanamount);
+			System.out.println("Remaining balnce left " + (loanamount)+"\n");
+		}
+
+		System.out.println("\nFinal amount owed: " + (loanamount));
 
 	}
 
